d35992bef0cb42752a0244c1f96e18bfc64fd9f5
Rai_Froh <raifroh@gmail.com>
2023-01-24 11:57:52 -0800

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 // 3.1: Phonebook backend step1
 // Implement a Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons.
 
+//Data to use
+// [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
